49c8790cc433491c988366ccec16c578a11768e3
JagTheFriend <JagTheFriend12@gmail.com>
2023-10-24 17:14:58 +0200

    Added more routes
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -11,20 +11,55 @@ export const postRouter = createTRPCRouter({
       };
     }),
 
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
+  createPost: publicProcedure
+    .input(
+      z.object({
+        title: z.string().min(1),
+        content: z.string().min(10),
+        authorId: z.string().min(1),
+      })
+    )
     .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      return ctx.db.post.create({
+      return await ctx.db.post.create({
         data: {
-          name: input.name,
+          title: input.title,
+          content: input.content,
+          authorId: input.authorId,
         },
       });
     }),
 
-  getLatest: publicProcedure.query(({ ctx }) => {
+  deletePost: publicProcedure
+    .input(
+      z.object({
+        authorId: z.string(),
+        title: z.string().min(1),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.db.post.deleteMany({
+        where: {
+          AND: [
+            {
+              title: input.title,
+              authorId: input.authorId,
+            },
+          ],
+        },
+      });
+    }),
+
+  deleteAllPostsByAuthor: publicProcedure
+    .input(z.object({ authorId: z.string() }))
+    .mutation(({ ctx, input }) => {
+      return ctx.db.post.deleteMany({
+        where: {
+          authorId: input.authorId,
+        },
+      });
+    }),
+
+  getPosts: publicProcedure.query(({ ctx }) => {
     return ctx.db.post.findFirst({
       orderBy: { createdAt: "desc" },
     });
