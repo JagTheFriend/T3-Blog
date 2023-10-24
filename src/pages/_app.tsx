df713bd6e38e026314ffba684e740dc0d5666e1d
JagTheFriend <JagTheFriend12@gmail.com>
2023-10-24 15:00:54 +0200

    Import bootstrap
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
