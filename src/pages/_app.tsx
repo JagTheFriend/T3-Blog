2fd31f108452a9ef2fac9a7f82602a9ff995c5b9
JagTheFriend <JagTheFriend12@gmail.com>
2023-10-25 12:52:04 +0200

    Fixed bug
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,13 @@ import { api } from "~/utils/api";
 import { ClerkProvider } from "@clerk/nextjs";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "~/styles/globals.css";
+import { Toaster } from "react-hot-toast";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
       <Component {...pageProps} />
+      <Toaster position="bottom-center" />
     </ClerkProvider>
   );
 };
