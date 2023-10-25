ea862b463a4ce2bb5802466c9291d04aeed4ddd6
JagTheFriend <JagTheFriend12@gmail.com>
2023-10-25 10:09:19 +0200

    Created function
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

---
+++ src/utils/addUserDataToPost.ts
@@ -0,0 +1,26 @@
+import { clerkClient } from "@clerk/nextjs";
+import type { Post } from "@prisma/client";
+import { TRPCError } from "@trpc/server";
+
+export async function addUserDataToPost(posts: Post[]) {
+  const userId = posts.map((post) => post.authorId);
+
+  const users = await clerkClient.users.getUserList({ userId, limit: 110 });
+
+  return posts.map((post) => {
+    const author = users.find((user) => user.id === post.authorId);
+    if (!author) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: `Author for post not found. POST ID: ${post.id}, USER ID: ${post.authorId}`,
+      });
+    }
+    return {
+      post,
+      author: {
+        ...author,
+        username: author.username ?? "Unknown",
+      },
+    };
+  });
+}
