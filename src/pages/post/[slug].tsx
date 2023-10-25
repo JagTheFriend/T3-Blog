ee5fc7ef232039431e502b9309773c66bef60739
JagTheFriend <JagTheFriend12@gmail.com>
2023-10-25 19:38:07 +0200

    Provide slug to ViewPost
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,5 +1,27 @@
-function ViewPost() {
-  return <></>;
+import type { GetStaticProps } from "next";
+import { Container } from "react-bootstrap";
+
+function ViewPost({ slug }: { slug: string }) {
+  return (
+    <>
+      <Container>{slug}</Container>
+    </>
+  );
 }
+
+export const getStaticProps: GetStaticProps = (context) => {
+  const slug = context.params?.slug;
+
+  if (typeof slug !== "string") throw new Error("no slug");
+  return {
+    props: {
+      slug,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
+};
 
 export default ViewPost;
