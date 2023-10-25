ff623a278b31b51ca6fbf62fb750ea7e510dad15
JagTheFriend <JagTheFriend12@gmail.com>
2023-10-25 10:21:31 +0200

    Only provide id, username, and profile image
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/utils/addUserDataToPost.ts
+++ src/utils/addUserDataToPost.ts
@@ -1,11 +1,22 @@
 import { clerkClient } from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/dist/types/server";
 import type { Post } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
+
+function filterUserForClient(user: User) {
+  return {
+    id: user.id,
+    username: user.username ?? "Unknown",
+    profileImageUrl: user.imageUrl,
+  };
+}
 
 export async function addUserDataToPost(posts: Post[]) {
   const userId = posts.map((post) => post.authorId);
 
-  const users = await clerkClient.users.getUserList({ userId, limit: 110 });
+  const users = (
+    await clerkClient.users.getUserList({ userId, limit: 110 })
+  ).map(filterUserForClient);
 
   return posts.map((post) => {
     const author = users.find((user) => user.id === post.authorId);
@@ -19,7 +30,7 @@ export async function addUserDataToPost(posts: Post[]) {
       post,
       author: {
         ...author,
-        username: author.username ?? "Unknown",
+        username: author.username,
       },
     };
   });
