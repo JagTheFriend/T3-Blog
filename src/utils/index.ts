a5e7f37ede6c8d381e64e374b21934b0a30401f9
JagTheFriend <JagTheFriend12@gmail.com>
2023-10-26 14:16:02 +0200

    Use first name and last name if username is unavailable
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,7 +3,8 @@ import type { User } from "@clerk/nextjs/dist/types/server";
 export function filterUserForClient(user: User) {
   return {
     id: user.id,
-    username: user.username ?? "Unknown",
+    username:
+      user.username ?? `${user.firstName} ${user.lastName}` ?? "Unknown",
     profileImageUrl: user.imageUrl,
   };
 }
