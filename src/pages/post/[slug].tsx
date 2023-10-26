c2ba6e9ca79a8fe2b9716e11d01e2f377f759df9
JagTheFriend <JagTheFriend12@gmail.com>
2023-10-26 11:59:24 +0200

    User can create comment now
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,8 +1,9 @@
+import { useUser } from "@clerk/nextjs";
 import type { TRPCError } from "@trpc/server";
 import type { GetStaticProps } from "next";
 import Head from "next/head";
-import { useEffect } from "react";
-import { Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Button, Container, FloatingLabel, Form } from "react-bootstrap";
 import toast from "react-hot-toast";
 import DisplayBlogContent, {
   type DataType,
@@ -10,6 +11,71 @@ import DisplayBlogContent, {
 import LoadingPage from "~/component/LoadingPage";
 import NavbarComponent from "~/component/Navbar";
 import { api } from "~/utils/api";
+
+type CreateCommentProps = {
+  postId: string;
+};
+
+function CreateComment({ postId }: CreateCommentProps) {
+  const { user } = useUser();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { mutate } = api.comment.createComment.useMutation({
+    onError: (error) => {
+      toast.error(error.message);
+      setIsLoading(false);
+    },
+    onSuccess: () => {
+      toast.success("Comment created successfully!");
+      setIsLoading(false);
+    },
+  });
+
+  const [comment, setComment] = useState("");
+
+  const postComment = () => {
+    if (comment.trim().length == 0) {
+      return toast.error("Provide comment");
+    }
+
+    if (!user) {
+      toast.error("Please sign-in to post the comment.");
+      return toast.error("Note: Copy comment before leaving this page");
+    }
+    setIsLoading(true);
+    mutate({
+      content: comment,
+      postId: postId,
+      authorId: user.id,
+    });
+  };
+
+  return (
+    <>
+      <FloatingLabel
+        controlId="commentInput"
+        label="Write a comment"
+        className="mb-2 mt-3"
+      >
+        <Form.Control
+          type="text"
+          disabled={isLoading}
+          placeholder="Write a comment"
+          onChange={(e) => {
+            setComment(e.target.value);
+          }}
+        />
+      </FloatingLabel>
+      <Button
+        variant="outline-success"
+        onClick={() => postComment()}
+        disabled={isLoading}
+      >
+        Post
+      </Button>
+    </>
+  );
+}
 
 function DisplayComments({ postId }: { postId: string }) {
   const { data, isError, isLoading } = api.comment.getComments.useQuery({
@@ -22,7 +88,15 @@ function DisplayComments({ postId }: { postId: string }) {
     }
   }, [isError]);
 
-  return <></>;
+  if (isLoading) {
+    return <LoadingPage />;
+  }
+
+  return (
+    <>
+      <CreateComment postId={postId} />
+    </>
+  );
 }
 
 type ViewPostDetailType = {
