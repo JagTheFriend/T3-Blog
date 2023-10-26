7ffcecc8507c418fe9ef6d6d7162205b624e96fe
JagTheFriend <JagTheFriend12@gmail.com>
2023-10-26 12:12:02 +0200

    Add user data to comments
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/utils/addUserDataToComment.ts
+++ src/utils/addUserDataToComment.ts
@@ -1,5 +1,29 @@
+import { clerkClient } from "@clerk/nextjs";
 import type { Comment } from "@prisma/client";
+import { TRPCError } from "@trpc/server";
+import { filterUserForClient } from ".";
 
 export async function addUserDataToComment(comments: Comment[]) {
-  return "";
+  const userId = comments.map((comment) => comment.authorId);
+
+  const users = (
+    await clerkClient.users.getUserList({ userId, limit: 110 })
+  ).map(filterUserForClient);
+
+  return comments.map((comment) => {
+    const author = users.find((user) => user.id === comment.authorId);
+    if (!author) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: `Author for comment not found. COMMENT ID: ${comment.id}, USER ID: ${comment.authorId}`,
+      });
+    }
+    return {
+      comment,
+      author: {
+        ...author,
+        username: author.username,
+      },
+    };
+  });
 }
