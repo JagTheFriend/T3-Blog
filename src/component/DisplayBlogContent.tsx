bbdf7fa52bad7dc1cc6e43cafc14f9920631bad2
JagTheFriend <JagTheFriend12@gmail.com>
2023-10-26 10:43:14 +0200

    New component to display blog content
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

---
+++ src/component/DisplayBlogContent.tsx
@@ -0,0 +1,58 @@
+import type { Post } from "@prisma/client";
+import { format } from "date-fns";
+import { sanitize } from "dompurify";
+import Link from "next/link";
+import { Converter } from "showdown";
+
+type DataType = {
+  post: Post;
+  author: {
+    username: string;
+    id: string;
+    profileImageUrl: string;
+  };
+  goBackUrl: string;
+};
+
+function DisplayBlogContent({ post, author, goBackUrl }: DataType) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  const date: string = format(new Date(post.createdAt), "dd/MM/yyyy");
+
+  const converter = new Converter();
+  const postContent = sanitize(converter.makeHtml(post.content));
+
+  return (
+    <>
+      <div
+        style={{
+          marginTop: "20px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <h3>{post.title}</h3>
+        By {author.username} at {date}
+      </div>
+      <hr />
+      Description: {post.description}
+      <hr />
+      <div dangerouslySetInnerHTML={{ __html: postContent }}></div>
+      <Link
+        href={goBackUrl}
+        style={{
+          background: "none !important",
+          border: "none",
+          padding: "0 !important",
+          color: "#069",
+          textDecoration: "underline",
+          cursor: "pointer",
+        }}
+      >
+        ← Go Back
+      </Link>
+    </>
+  );
+}
+
+export default DisplayBlogContent;
