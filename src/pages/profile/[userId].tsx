18a98b373ed4e592d05c0a860fe06de38a5892af
JagTheFriend <JagTheFriend12@gmail.com>
2023-10-27 09:51:26 +0200

    It can now display posts make by user
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/pages/profile/[userId].tsx
+++ src/pages/profile/[userId].tsx
@@ -1,11 +1,33 @@
 import type { GetStaticProps } from "next";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
+import LoadingPage from "~/component/LoadingPage";
 import NavbarComponent from "~/component/Navbar";
+import { api } from "~/utils/api";
+import { DisplayData } from "..";
 
 function GetPostsByUserId({ userId }: { userId: string }) {
+  const { data, isLoading, isError, isFetched } =
+    api.post.getPostByUserId.useQuery({
+      userId,
+    });
+
+  useEffect(() => {
+    if (isError && isFetched) {
+      toast.error("Unable to retrieve posts. Please try again later");
+    }
+  }, [isError, isFetched]);
+
   return (
     <>
       <NavbarComponent />
-      {userId}
+      {isLoading ? (
+        <LoadingPage />
+      ) : (
+        <div className="mt-3">
+          <DisplayData data={data} />
+        </div>
+      )}
     </>
   );
 }
